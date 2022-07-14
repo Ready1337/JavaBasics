8a963c664d5a5ff1905f1f69217f4b7440f625af
Ready1337 <egor.zinovjev2013@yandex.ru>
2022-07-14 18:28:05 +0500

    StringConcatenation.java added

---
+++ src/StringConcatenation/StringConcatenation.java
@@ -0,0 +1,13 @@
+package StringConcatenation;
+
+public class StringConcatenation {
+
+    public static void main(String[] Args) {
+        // Конкатенация строк: https://vertex-academy.com/tutorials/ru/konkatenaciya-strok/
+        String morning1 ="доброе";
+        String morning2 ="не доброе, потому что сегодня понедельник";
+
+        System.out.println("Сегодня утро " + morning1);
+        System.out.println("Сегодня утро " + morning2);
+    }
+}
