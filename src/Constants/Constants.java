dc1d47945d0a89fc324b2a23d10b77372b7b7a57
Ready1337 <egor.zinovjev2013@yandex.ru>
2022-07-14 18:44:22 +0500

    Constants.java added

---
+++ src/Constants/Constants.java
@@ -0,0 +1,18 @@
+package Constants;
+
+public class Constants {
+
+    public static void main(String[] Args){
+        /*
+        Константы в Java: https://vertex-academy.com/tutorials/ru/konstanty-v-java/
+         */
+
+        /*
+        Константы принято объявлять с большой буквы
+        Объявление констант: final <тип> <название>;
+         */
+        final int ExampleOfIntConstantVariable = 1;
+
+        System.out.println(ExampleOfIntConstantVariable + 1);
+    }
+}
