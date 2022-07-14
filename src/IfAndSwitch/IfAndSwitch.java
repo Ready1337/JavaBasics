9c48138efa57f1db72521972748f8e2ac7d949e2
Ready1337 <egor.zinovjev2013@yandex.ru>
2022-07-14 19:13:26 +0500

    IfAndSwitch.java added

---
+++ src/IfAndSwitch/IfAndSwitch.java
@@ -0,0 +1,10 @@
+package IfAndSwitch;
+
+public class IfAndSwitch {
+
+    public static void main(String[] Args) {
+        /*
+        Условный оператор if в Java. Оператор switch: https://vertex-academy.com/tutorials/ru/vetvlenie-v-java/
+         */
+    }
+}
