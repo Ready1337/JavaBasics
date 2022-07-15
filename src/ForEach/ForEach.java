4b2e5ed84736b693d464eea4f6a662844e55775c
Ready1337 <egor.zinovjev2013@yandex.ru>
2022-07-15 13:12:45 +0500

    ForEach.java with Task1 added

---
+++ src/ForEach/ForEach.java
@@ -0,0 +1,10 @@
+package ForEach;
+
+public class ForEach {
+
+    public static void main(String[] Args) {
+        /*
+        Цикл for each: https://vertex-academy.com/tutorials/ru/cikl-for-each/
+         */
+    }
+}
