2ed61079ef08a4bf6965dd54fa31fa76f2b35c2b
Ready1337 <egor.zinovjev2013@yandex.ru>
2022-07-19 10:17:39 +0500

    Classes.java added

---
+++ src/Basics/Classes/Classes.java
@@ -0,0 +1,15 @@
+package Basics.Classes;
+
+import Basics.Classes.Classes;
+public class Classes {
+
+    public static void main(String[] Args) {
+        /*
+        Классы в Java: https://vertex-academy.com/tutorials/ru/klassy-v-java/
+         */
+
+        ExampleClass objectOfExampleClass = new ExampleClass();
+
+        System.out.println(objectOfExampleClass.exampleStringField);
+    }
+}
---
+++ src/Basics/Classes/ExampleClass.java
@@ -0,0 +1,6 @@
+package Basics.Classes;
+
+public class ExampleClass {
+
+    String exampleStringField = "I am an example string";
+}
